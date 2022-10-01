770246513899de86347aa694b973a1f3536b7743
Daniil <danya055555@gmail.com>
2022-10-01 23:01:49 +0300

    A.cpp

--- A.cpp
+++ A.cpp
@@ -2,54 +2,46 @@
 
 #include <vector>
 
-void merge(std::vector<int>* vec, int n, int mid, int m);
+void merge(std::vector<int> vec, int left, int mid, int right);
 
 void merge_sort(std::vector<int> vec, int left, int right) {
-  int n = right - left;
-  int mid = left + (right - left) / 2;
-  if (n > 1) {
-    merge_sort(vec,left, mid);
-    merge_sort(vec, mid, right);
-    std::vector<int> vec1(vec.begin()+left, vec.begin()+mid);
-    std::vector<int> vec2(vec.begin()+mid, vec.begin() + right);
-    int i = left;
-    int j = mid;
-    while (i < mid and j < right) {
-      if (vec1[i] < vec2[j]) {
-        vec[i + j] = vec1[i];
-        ++i;
-      } else {
-        vec[i + j] = vec1[i];
-        ++j;
-      }
+  int mid;
+  if (left < right) {
+    mid = (right + left) / 2;
+    merge_sort(vec, left, mid);
+    merge_sort(vec, mid + 1, right);
+    merge(vec, left, mid, right);
+  }
+}
+
+void merge(std::vector<int> vec, int left, int mid, int right) {
+  std::vector<int> vec1(vec.begin() + left, vec.begin() + mid);
+  std::vector<int> vec2(vec.begin() + mid, vec.begin() + right);
+  int i, j;
+  for(i = 0, j = 0; i < mid - left and j < right - mid; ++i, ++j) {
+    if (vec1[i] < vec2[j]) {
+      vec[left++] = vec1[i];
+    } else {
+      vec[left++] = vec2[i];
     }
-    while (i < mid) {
-      vec[i + j] = vec1[i];
-      ++i;
-    }
-    while (j < right) {
-      vec[i + j] = vec1[i];
-      ++j;
-    }
+  }
+  while (i < vec1.size()) {
+    vec[left++] = vec1[i++];
+  }
+  while (j < vec2.size()) {
+    vec[left++] = vec2[j++];
   }
 }
 
 int main() {
   int N;
   std::cin >> N;
-  std::vector<int> vec(N);
+  std::vector<int> vec(2*N);
   for (int i = 0; i < 2*N; i += 2) {
-    int a;
-    int b;
-    std::cin >> a >> b;
-    if (!vec.empty()){
-      for (int j = 0; j < vec.size(); ++j) {
-        if (a <= vec[j]) {
-
-        }
-      }
-    }
-    vec[i] = a;
-    vec[i + 1] = b;
+    std::cin >> vec[i] >> vec[i + 1];
+  }
+  merge_sort(vec, 0, 2*N - 1);
+  for (int i = 0; i < 2*N; i += 2) {
+    std::cout << vec[i] << "\t" << vec[i + 1] << '\n';
   }
 }
