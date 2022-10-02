3d3c7e00c950b81dff1302a102bdff9494d7c714
Daniil <danya055555@gmail.com>
2022-10-02 16:54:45 +0300

    A.cpp

--- A.cpp
+++ A.cpp
@@ -2,46 +2,47 @@
 
 #include <vector>
 
-void merge(std::vector<int> vec, int left, int mid, int right);
+void merge(std::vector<int>& vec, int left, int mid, int right) {
+  std::vector<int> merge_vector;
+  int i = left;
+  int j = mid + 1;
+  while (i <= mid and j <= right) {
+    if (vec[i] <= vec[j]) {
+      merge_vector.push_back(vec[i++]);
+    } else {
+      merge_vector.push_back(vec[j++]);
+    }
+  }
+  while (i <= mid) {
+    merge_vector.push_back(vec[i++]);
+  }
+  while (j <= right) {
+    merge_vector.push_back(vec[j++]);
+  }
+  for (int t = left; t <= right; ++t) {
+    vec[t] = merge_vector[t-left];
+  }
+}
 
-void merge_sort(std::vector<int> vec, int left, int right) {
-  int mid;
+void merge_sort(std::vector<int>& vec, int left, int right) {
   if (left < right) {
-    mid = (right + left) / 2;
+    int mid = (right + left) / 2;
     merge_sort(vec, left, mid);
     merge_sort(vec, mid + 1, right);
     merge(vec, left, mid, right);
   }
 }
 
-void merge(std::vector<int> vec, int left, int mid, int right) {
-  std::vector<int> vec1(vec.begin() + left, vec.begin() + mid);
-  std::vector<int> vec2(vec.begin() + mid, vec.begin() + right);
-  int i, j;
-  for(i = 0, j = 0; i < mid - left and j < right - mid; ++i, ++j) {
-    if (vec1[i] < vec2[j]) {
-      vec[left++] = vec1[i];
-    } else {
-      vec[left++] = vec2[i];
-    }
-  }
-  while (i < vec1.size()) {
-    vec[left++] = vec1[i++];
-  }
-  while (j < vec2.size()) {
-    vec[left++] = vec2[j++];
-  }
-}
-
 int main() {
   int N;
   std::cin >> N;
-  std::vector<int> vec(2*N);
-  for (int i = 0; i < 2*N; i += 2) {
-    std::cin >> vec[i] >> vec[i + 1];
+  std::vector<int> vec1(N);
+  std::vector<int> vec2(N);
+  std::vector<int> res;
+  for (int i = 0; i < N; i++) {
+    std::cin >> vec1[i] >> vec2[i];
   }
-  merge_sort(vec, 0, 2*N - 1);
-  for (int i = 0; i < 2*N; i += 2) {
-    std::cout << vec[i] << "\t" << vec[i + 1] << '\n';
-  }
+  merge_sort(vec1, 0, N);
+  merge_sort(vec2, 0, N);
+  
 }
