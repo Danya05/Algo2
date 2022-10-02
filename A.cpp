31b8ef99fbb0e65645cc9c285048b3b97ad33c93
Daniil <danya055555@gmail.com>
2022-10-02 18:11:21 +0300

    A.cpp

--- A.cpp
+++ A.cpp
@@ -2,10 +2,11 @@
 
 #include <vector>
 
-void merge(std::vector<int>& vec, int left, int mid, int right) {
-  std::vector<int> merge_vector;
-  int i = left;
-  int j = mid + 1;
+void Merge(std::vector<long long>& vec, long long left, long long mid,
+           long long right) {
+  std::vector<long long> merge_vector;
+  long long i = left;
+  long long j = mid + 1;
   while (i <= mid and j <= right) {
     if (vec[i] <= vec[j]) {
       merge_vector.push_back(vec[i++]);
@@ -19,30 +20,41 @@ void merge(std::vector<int>& vec, int left, int mid, int right) {
   while (j <= right) {
     merge_vector.push_back(vec[j++]);
   }
-  for (int t = left; t <= right; ++t) {
-    vec[t] = merge_vector[t-left];
+  for (long long t = left; t <= right; ++t) {
+    vec[t] = merge_vector[t - left];
   }
 }
 
-void merge_sort(std::vector<int>& vec, int left, int right) {
+void MergeSort(std::vector<long long>& vec, long long left, long long right) {
   if (left < right) {
-    int mid = (right + left) / 2;
-    merge_sort(vec, left, mid);
-    merge_sort(vec, mid + 1, right);
-    merge(vec, left, mid, right);
+    long long mid = (right + left) / 2;
+    MergeSort(vec, left, mid);
+    MergeSort(vec, mid + 1, right);
+    Merge(vec, left, mid, right);
   }
 }
 
 int main() {
-  int N;
-  std::cin >> N;
-  std::vector<int> vec1(N);
-  std::vector<int> vec2(N);
-  std::vector<int> res;
-  for (int i = 0; i < N; i++) {
-    std::cin >> vec1[i] >> vec2[i];
+  long long n, k, start0, start1;
+  std::cin >> n >> k >> start0 >> start1;
+  std::vector<long long> vec;
+  vec.push_back(start0);
+  vec.push_back(start1);
+  long long temp_1 = (long long)vec[0];
+  long long temp_2 = (long long)vec[1];
+  long long curr;
+  for (long long i = 2; i < n; i++) {
+    curr = (temp_2 * 123 + temp_1 * 45) % (10000000 + 4321);
+    if ((long long)vec.size() < k) {
+      vec.push_back(curr);
+    } else {
+      if (curr < vec[k - 1]) {
+        vec[k - 1] = curr;
+        MergeSort(vec, 0, k);
+      }
+    }
+    temp_1 = temp_2;
+    temp_2 = curr;
   }
-  merge_sort(vec1, 0, N);
-  merge_sort(vec2, 0, N);
-  
+  std::cout << vec[k - 1];
 }
