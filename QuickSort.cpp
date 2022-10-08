0200ea9d0cbc64e343feed3bc72fc5836b7872ac
Danya05 <112342386+Danya05@users.noreply.github.com>
2022-10-08 13:08:11 +0300

    Update QuickSort.cpp

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -2,7 +2,8 @@
 
 #include <vector>
 
-long long Partition(std::vector<long long>& vec, long long left, long long right) {
+long long Partition(std::vector<long long>& vec, long long left,
+                    long long right) {
   long long pivot = vec[(left + right) / 2];
   long long i = left;
   long long j = right;
@@ -23,21 +24,6 @@ long long Partition(std::vector<long long>& vec, long long left, long long right
   return j;
 }
 
-void sub_sort(std::vector<long long>& vec, long long n, long long dist) {
-  long long t = n;
-  bool not_sorted = true;
-  for (long long i = 0; i < n and not_sorted; i += dist) {
-    not_sorted = false;
-    for (long long j = dist; j < t; j += dist) {
-      if (vec[j - dist] > vec[j]) {
-        std::swap(vec[j - dist], vec[j]);
-        not_sorted = true;
-      }
-    }
-    t -= dist;
-  }
-}
-
 void Qsort(std::vector<long long>& vec, long long left, long long right) {
   if (left >= right) {
     return;
@@ -47,23 +33,15 @@ void Qsort(std::vector<long long>& vec, long long left, long long right) {
   Qsort(vec, p + 1, right);
 }
 
-long long Kstat(std::vector<long long>& vec, long long k) {
-  long long left = 1;
-  long long right = (long long)vec.size();
-  while (true) {
-    long long mid = Partition(vec, left, right);
-    if (mid == k) {
-      return mid;
-    }
-    if (mid < k) {
-      left = mid + 1;
-    }
-    if (mid > k) {
-      right = mid;
-    }
-  }
-}
-
 int main() {
-  
+  long long n;
+  std::cin >> n;
+  std::vector<long long> vec(n);
+  for (long long i = 0; i < n; ++i) {
+    std::cin >> vec[i];
+  }
+  Qsort(vec, 0, n - 1);
+  for (long long i = 0; i < n; ++i) {
+    std::cout << vec[i] << " ";
+  }
 }
